92de6f82c0115366c9314339440517b56654cfbd
Lodewic <lodewic.van.twillert@schiphol.nl>
2023-08-24 16:07:10 +0200

    implement working dynamic pipeline example using hooks and data factories

--- src/kedro_dynamic_pipeline_hook_example/nodes.py
+++ src/kedro_dynamic_pipeline_hook_example/nodes.py
@@ -1,13 +1,8 @@
-"""
-This is a boilerplate pipeline
-generated using Kedro 0.18.12
-"""
-
 import logging
 from typing import Any, Dict, Tuple
 
-import numpy as np
 import pandas as pd
+from kedro.utils import load_obj
 
 
 def split_data(
@@ -34,41 +29,16 @@ def split_data(
 
     return X_train, X_test, y_train, y_test
 
+def fit_model(X: pd.DataFrame, y: pd.Series, model_type: str, model_kwargs: dict[str, Any] = {}):
+    """Loads model class from `model_type` and fits model to X and y data."""
+    model_class = load_obj(model_type)
+    model_obj = model_class(**model_kwargs)
+    model_obj.fit(X, y)
+    return model_obj
 
-def make_predictions(
-    X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series
-) -> pd.Series:
-    """Uses 1-nearest neighbour classifier to create predictions.
-
-    Args:
-        X_train: Training data of features.
-        y_train: Training data for target.
-        X_test: Test data for features.
-
-    Returns:
-        y_pred: Prediction of the target variable.
-    """
-
-    X_train_numpy = X_train.to_numpy()
-    X_test_numpy = X_test.to_numpy()
-
-    squared_distances = np.sum(
-        (X_train_numpy[:, None, :] - X_test_numpy[None, :, :]) ** 2, axis=-1
-    )
-    nearest_neighbour = squared_distances.argmin(axis=0)
-    y_pred = y_train.iloc[nearest_neighbour]
-    y_pred.index = X_test.index
-
-    return y_pred
-
-
-def report_accuracy(y_pred: pd.Series, y_test: pd.Series):
-    """Calculates and logs the accuracy.
-
-    Args:
-        y_pred: Predicted target.
-        y_test: True target.
-    """
-    accuracy = (y_pred == y_test).sum() / len(y_test)
+def score_model(X: pd.DataFrame, y: pd.Series, model):
+    """Score model on X and y dataset, assuming model has a .score(X, y) method."""
+    score = model.score(X, y)
     logger = logging.getLogger(__name__)
-    logger.info("Model has accuracy of %.3f on test data.", accuracy)
+    logger.info(f"{model=} has score of %.3f.", score)
+    return score
--- src/kedro_dynamic_pipeline_hook_example/pipeline_registry.py
+++ src/kedro_dynamic_pipeline_hook_example/pipeline_registry.py
@@ -1,16 +1,51 @@
-"""Project pipelines."""
-from typing import Dict
-
-from kedro.framework.project import find_pipelines
+"""Project dynamic pipelines."""
+from kedro.io import DataCatalog
 from kedro.pipeline import Pipeline
 
+from kedro_dynamic_pipeline_hook_example.pipeline import (
+    create_namespaced_model_pipeline,
+    create_split_data_pipeline,
+)
 
-def register_pipelines() -> Dict[str, Pipeline]:
-    """Register the project's pipelines.
+
+def register_pipelines():
+    """Method that will be assigned to the callable returned by register_dynamic_pipelines(...), by a Hook."""
+
+    raise NotImplementedError("""
+    register_pipelines() is expected to be overwritten by ProjectHooks.
+    Make sure the hooks is found in hooks.py and enabled in settings.py
+    """)
+
+
+def register_dynamic_pipelines(catalog: DataCatalog) -> dict[str, Pipeline]:
+    """Register the project's pipelines depending on the catalog.
+
+    Create pipelines dynamically based on parameters and datasets defined in the catalog.
+    The function must return a callable without any arguments that will replace the
+    `register_pipelines()` method in this same module, using an `after_catalog_created_hook`.
+
+    Args:
+        catalog: The DataCatalog loaded from the KedroContext.
 
     Returns:
-        A mapping from pipeline names to ``Pipeline`` objects.
+        A callable that returns a mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    # create pipelines with access to catalog
+    split_data_pipe = create_split_data_pipeline()
+    model_pipe = create_namespaced_model_pipeline(catalog=catalog)
+
+    def register_pipelines():
+        """Register the project's pipelines.
+
+        Returns:
+            A mapping from pipeline names to ``Pipeline`` objects.
+        """
+        pipelines = {
+            "split_data": split_data_pipe,
+            "models": model_pipe,
+            "__default__": split_data_pipe + model_pipe
+        }
+
+        return pipelines
+
+    return register_pipelines
